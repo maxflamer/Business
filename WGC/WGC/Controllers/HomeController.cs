ec0e4c204306f2ca55ad43510570062a990b08aa
maxflamer <km_95@hotmail.com>
2019-02-27 02:06:14 +0530

    Created Model for feedback message and Post ActionMethod for receiving it

--- WGC/WGC/Controllers/HomeController.cs
+++ WGC/WGC/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WGC.Models;
 
 namespace WGC.Controllers
 {
@@ -12,7 +13,23 @@ namespace WGC.Controllers
         public ActionResult Index()
         {
             ViewBag.Title = "WGC";
-            return View();
+            FeedbackMessage feedbackMessage = new FeedbackMessage();
+            return View(feedbackMessage);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Message(FeedbackMessage feedbackMessage)
+        {
+
+            if (ModelState.IsValid)
+            {
+
+                //its valid, update your database or do soemthing useful here
+                return RedirectToAction("Success");
+            }
+            //its not valid reload the page and let data annotations show the error
+            return View(feedbackMessage);
         }
     }
 }
---
+++ WGC/WGC/Models/FeedbackMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WGC.Models
+{
+    public class FeedbackMessage
+    {
+        public string Name { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Message { get; set; }
+    }
+}
